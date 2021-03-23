2142c7ffced92e04e20ad57234d7b83cdf5391d9
judiazp <judiazp2p@gmail.com>
2021-03-22 21:19:17 -0300

    Creando el boton de logout

---
+++ src/components/atoms/buttonLogout/buttonLogout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './buttonLogout.css'
+
+const btnLogout = () => {
+    return (
+        <div className = "btns">
+            <a href ="#"  className = "buttons">Cerrar sesión</a>
+        </div>
+    )
+}
+
+export default btnLogout;
